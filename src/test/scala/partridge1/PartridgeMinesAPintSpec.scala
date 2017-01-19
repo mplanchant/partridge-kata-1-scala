83862e3db3a3939f68bf4053780eda477d2d857d
Matthew Planchant <matthew.planchant@digital.hmrc.gov.uk>
2017-01-19 13:28:41 +0000

    Modifies test assertions.

--- src/test/scala/partridge1/PartridgeMinesAPintSpec.scala
+++ src/test/scala/partridge1/PartridgeMinesAPintSpec.scala
@@ -13,19 +13,19 @@ class PartridgeMinesAPintSpec extends AlanSpec with Matchers {
   }
 
   he should "say Mine's a Pint! if the list contains a single partridge term" in {
-    PartridgeMinesAPint(List("PearTree")) shouldBe "Mine's a Pint!"
+    PartridgeMinesAPint(List("PearTree")) should endWith ("Pint!")
   }
 
   he should "say Mine's a Pint!! if the list contains a two partridge terms" in {
-    PartridgeMinesAPint(List("PearTree", "PearTree")) shouldBe "Mine's a Pint!!"
+    PartridgeMinesAPint(List("PearTree", "PearTree")) should endWith ("Pint!!")
   }
 
   he should "say Mine's a Pint!!!!!!!! if the list contains all the partridge terms" in {
-    PartridgeMinesAPint(List("Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad")) shouldBe "Mine's a Pint!!!!!!!!"
+    PartridgeMinesAPint(List("Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad")) should endWith ("Pint!!!!!!!!")
   }
 
   he should "append the correct number of exclamation marks if the list contains a mixture of partridge an non-partridge terms" in {
-    PartridgeMinesAPint(List("Partridge", "PearTree", "Chat", "Dan", "Blue", "Green", "Yellow", "Red", "Toblerone", "Lynn", "AlphaPapa", "Nomad")) shouldBe "Mine's a Pint!!!!!!!!"
+    PartridgeMinesAPint(List("Partridge", "PearTree", "Chat", "Dan", "Blue", "Green", "Yellow", "Red", "Toblerone", "Lynn", "AlphaPapa", "Nomad")) should endWith ("Pint!!!!!!!!")
   }
 }
 
